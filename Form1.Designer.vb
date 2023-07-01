a1cd36bca40e2a75408f21a2d09024296e6b4824
zeebi <zeebi@LEONA-DESKTOP>
2023-07-02 01:24:36 +1000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.CheckBox120FPS = New System.Windows.Forms.CheckBox()
+        Me.CheckBox60FPS = New System.Windows.Forms.CheckBox()
+        Me.CheckBox30FPS = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Res480p = New System.Windows.Forms.CheckBox()
+        Me.Res1080p = New System.Windows.Forms.CheckBox()
+        Me.Res720p = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'CheckBox120FPS
+        '
+        Me.CheckBox120FPS.AutoSize = True
+        Me.CheckBox120FPS.Location = New System.Drawing.Point(271, 37)
+        Me.CheckBox120FPS.Name = "CheckBox120FPS"
+        Me.CheckBox120FPS.Size = New System.Drawing.Size(67, 17)
+        Me.CheckBox120FPS.TabIndex = 7
+        Me.CheckBox120FPS.Text = "120 FPS"
+        Me.CheckBox120FPS.UseVisualStyleBackColor = True
+        '
+        'CheckBox60FPS
+        '
+        Me.CheckBox60FPS.AutoSize = True
+        Me.CheckBox60FPS.Checked = True
+        Me.CheckBox60FPS.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox60FPS.Location = New System.Drawing.Point(204, 37)
+        Me.CheckBox60FPS.Name = "CheckBox60FPS"
+        Me.CheckBox60FPS.Size = New System.Drawing.Size(61, 17)
+        Me.CheckBox60FPS.TabIndex = 6
+        Me.CheckBox60FPS.Text = "60 FPS"
+        Me.CheckBox60FPS.UseVisualStyleBackColor = True
+        '
+        'CheckBox30FPS
+        '
+        Me.CheckBox30FPS.AutoSize = True
+        Me.CheckBox30FPS.Location = New System.Drawing.Point(137, 37)
+        Me.CheckBox30FPS.Name = "CheckBox30FPS"
+        Me.CheckBox30FPS.Size = New System.Drawing.Size(61, 17)
+        Me.CheckBox30FPS.TabIndex = 5
+        Me.CheckBox30FPS.Text = "30 FPS"
+        Me.CheckBox30FPS.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(35, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Res480p
+        '
+        Me.Res480p.AutoSize = True
+        Me.Res480p.Location = New System.Drawing.Point(137, 60)
+        Me.Res480p.Name = "Res480p"
+        Me.Res480p.Size = New System.Drawing.Size(69, 17)
+        Me.Res480p.TabIndex = 10
+        Me.Res480p.Text = "Res480p"
+        Me.Res480p.UseVisualStyleBackColor = True
+        '
+        'Res1080p
+        '
+        Me.Res1080p.AutoSize = True
+        Me.Res1080p.Checked = True
+        Me.Res1080p.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.Res1080p.Location = New System.Drawing.Point(271, 60)
+        Me.Res1080p.Name = "Res1080p"
+        Me.Res1080p.Size = New System.Drawing.Size(75, 17)
+        Me.Res1080p.TabIndex = 9
+        Me.Res1080p.Text = "Res1080p"
+        Me.Res1080p.UseVisualStyleBackColor = True
+        '
+        'Res720p
+        '
+        Me.Res720p.AutoSize = True
+        Me.Res720p.Location = New System.Drawing.Point(204, 60)
+        Me.Res720p.Name = "Res720p"
+        Me.Res720p.Size = New System.Drawing.Size(69, 17)
+        Me.Res720p.TabIndex = 8
+        Me.Res720p.Text = "Res720p"
+        Me.Res720p.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Res480p)
+        Me.Controls.Add(Me.Res1080p)
+        Me.Controls.Add(Me.Res720p)
+        Me.Controls.Add(Me.CheckBox120FPS)
+        Me.Controls.Add(Me.CheckBox60FPS)
+        Me.Controls.Add(Me.CheckBox30FPS)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CheckBox120FPS As CheckBox
+    Friend WithEvents CheckBox60FPS As CheckBox
+    Friend WithEvents CheckBox30FPS As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Res480p As CheckBox
+    Friend WithEvents Res1080p As CheckBox
+    Friend WithEvents Res720p As CheckBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,236 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports System.Windows.Forms
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim selectedFPS As Integer
+        Dim selectedformWidth As Integer
+        Dim selectedformHeight As Integer
+
+
+        If Res480p.Checked Then
+            selectedformWidth = 640
+            selectedformHeight = 480
+        ElseIf Res720p.Checked Then
+            selectedformWidth = 1280
+            selectedformHeight = 720
+        ElseIf Res1080p.Checked Then
+            selectedformWidth = 1920
+            selectedformHeight = 1080
+        End If
+
+        If CheckBox30FPS.Checked Then
+            selectedFPS = 30
+        ElseIf CheckBox60FPS.Checked Then
+            selectedFPS = 60
+        ElseIf CheckBox120FPS.Checked Then
+            selectedFPS = 120
+        End If
+
+        Dim ballForm As New BouncingBallForm(selectedFPS, selectedformWidth, selectedformHeight)
+        ballForm.Show()
+    End Sub
+
+    Private Sub CheckBox60FPS_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox60FPS.CheckedChanged
+        CheckBox30FPS.Checked = False
+        CheckBox120FPS.Checked = False
+
+    End Sub
+
+    Private Sub CheckBox30FPS_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox30FPS.CheckedChanged
+        CheckBox60FPS.Checked = False
+        CheckBox120FPS.Checked = False
+
+    End Sub
+
+    Private Sub CheckBox120FPS_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox120FPS.CheckedChanged
+        CheckBox30FPS.Checked = False
+        CheckBox60FPS.Checked = False
+
+    End Sub
+
+    Private Sub Res1080p_CheckedChanged(sender As Object, e As EventArgs) Handles Res1080p.CheckedChanged
+        Res480p.Checked = False
+        Res720p.Checked = False
+
+
+
+    End Sub
+
+    Private Sub Res720p_CheckedChanged(sender As Object, e As EventArgs) Handles Res720p.CheckedChanged
+        Res480p.Checked = False
+        Res1080p.Checked = False
+
+
+    End Sub
+
+    Private Sub Res480p_CheckedChanged(sender As Object, e As EventArgs) Handles Res480p.CheckedChanged
+        Res720p.Checked = False
+        Res1080p.Checked = False
+
+    End Sub
+End Class
+
+Public Class BouncingBallForm
+    Inherits Form
+    Private ballAlive As Boolean = True ' Flag to indicate if the ball is alive
+    Private collisionOccurred As Boolean = False ' Flag to track if collision has already occurred
+
+
+    Private ballPosition As New Point(0, 0) ' Initialize ball position to (0, 0)
+    Private ballVelocity As New Point(0, 0) ' Initialize ball velocity to (0, 0)
+    Private ballRadius As Integer = 20
+    Private timer As Timer
+    Private fps As Integer
+    Private speed As Integer = 5
+    Private sunRadius As Integer = 50
+    Private sunPosition As Point
+    Private sunColor As Color = Color.Yellow
+    Private shadowColor As Color = Color.FromArgb(100, Color.Gray) ' Shadow color
+    Private shadowOpacityFactor As Single = 0.5 ' Shadow opacity factor
+    Private shadowSizeFactor As Single = 0.8 ' Shadow size factor
+    Private momentum As Size ' Stores the last velocity
+    Private gameOver As Boolean = False
+
+    Public Sub New(selectedFPS As Integer, formWidth As Integer, formHeight As Integer)
+
+        Me.DoubleBuffered = True
+        Me.ClientSize = New Size(formWidth, formHeight)
+        Me.StartPosition = FormStartPosition.CenterScreen
+
+        fps = selectedFPS
+        Dim interval As Integer = CInt(1000 / fps)
+        timer = New Timer()
+        timer.Interval = interval
+        AddHandler timer.Tick, AddressOf Timer_Tick
+        timer.Start()
+    End Sub
+
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        MyBase.OnPaint(e)
+
+        Dim graphics As Graphics = e.Graphics
+        graphics.Clear(Color.White)
+
+        ' Update the sun position to be in the center of the form
+        sunPosition = New Point(ClientSize.Width / 2, ClientSize.Height / 2)
+
+        ' Draw the sun
+        Dim sunBrush As New SolidBrush(sunColor)
+        Dim sunRect As New Rectangle(sunPosition.X - sunRadius, sunPosition.Y - sunRadius, sunRadius * 2, sunRadius * 2)
+        graphics.FillEllipse(sunBrush, sunRect)
+
+        ' Calculate the angle between the ball and the sun
+        Dim angle As Single = CSng(Math.Atan2(ballPosition.Y - sunPosition.Y, ballPosition.X - sunPosition.X))
+
+        ' Update the shadow color based on the angle
+        If angle >= 0 AndAlso angle <= Math.PI Then
+            shadowColor = Color.FromArgb(100, Color.Gray) ' Shadow color when ball is on the left side of the sun
+        Else
+            shadowColor = Color.FromArgb(50, Color.Gray) ' Shadow color when ball is on the right side of the sun
+        End If
+
+        ' Calculate the distance between the ball and the sun
+        Dim distance As Single = Math.Sqrt((ballPosition.X - sunPosition.X) ^ 2 + (ballPosition.Y - sunPosition.Y) ^ 2)
+
+        ' Calculate the shadow position and size based on the ball's position relative to the sun
+        Dim shadowOffsetX As Integer = CInt(Math.Cos(angle) * distance)
+        Dim shadowOffsetY As Integer = CInt(Math.Sin(angle) * distance)
+        Dim shadowRect As New Rectangle(ballPosition.X + shadowOffsetX - ballRadius, ballPosition.Y + shadowOffsetY - ballRadius, ballRadius * 2, ballRadius * 2)
+
+        ' Create a GraphicsPath to define the shape of the shadow
+        Dim shadowPath As New GraphicsPath()
+        shadowPath.AddEllipse(shadowRect)
+
+        ' Create a PathGradientBrush using the shadowPath to fill the shadow area
+        Dim pathBrush As New PathGradientBrush(shadowPath)
+        pathBrush.CenterColor = shadowColor
+        pathBrush.SurroundColors = {Color.Transparent}
+
+        ' Fill the shadow area with the pathBrush
+        graphics.FillPath(pathBrush, shadowPath)
+
+        ' Draw the ball
+        Dim ballBrush As New SolidBrush(Color.Red)
+        Dim ballRect As New Rectangle(ballPosition.X - ballRadius, ballPosition.Y - ballRadius, ballRadius * 2, ballRadius * 2)
+        graphics.FillEllipse(ballBrush, ballRect)
+    End Sub
+
+    Protected Overrides Sub OnKeyDown(e As KeyEventArgs)
+        MyBase.OnKeyDown(e)
+
+        Select Case e.KeyCode
+            Case Keys.W
+                momentum = New Size(momentum.Width, -speed)
+            Case Keys.A
+                momentum = New Size(-speed, momentum.Height)
+            Case Keys.S
+                momentum = New Size(momentum.Width, speed)
+            Case Keys.D
+                momentum = New Size(speed, momentum.Height)
+        End Select
+
+        Invalidate() ' Redraw the form to update the ball's position
+    End Sub
+
+    Protected Overrides Sub OnKeyUp(e As KeyEventArgs)
+        MyBase.OnKeyUp(e)
+
+        ' Reset the ball's velocity to the stored momentum
+        ballVelocity = momentum
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs)
+        ' Update the ball's position based on the velocity if it is alive
+        If ballAlive Then
+            ballPosition.X += ballVelocity.X
+            ballPosition.Y += ballVelocity.Y
+        End If
+
+        ' Check for collision with the form boundaries
+        If ballPosition.X < ballRadius Then
+            ballPosition.X = ballRadius
+            ballVelocity.X *= -1 ' Reverse the horizontal direction
+        ElseIf ballPosition.X > ClientSize.Width - ballRadius Then
+            ballPosition.X = ClientSize.Width - ballRadius
+            ballVelocity.X *= -1 ' Reverse the horizontal direction
+        End If
+
+        If ballPosition.Y < ballRadius Then
+            ballPosition.Y = ballRadius
+            ballVelocity.Y *= -1 ' Reverse the vertical direction
+        ElseIf ballPosition.Y > ClientSize.Height - ballRadius Then
+            ballPosition.Y = ClientSize.Height - ballRadius
+            ballVelocity.Y *= -1 ' Reverse the vertical direction
+        End If
+
+        ' Check for collision with the sun
+        Dim distance As Double = Math.Sqrt((ballPosition.X - sunPosition.X) ^ 2 + (ballPosition.Y - sunPosition.Y) ^ 2)
+        If distance <= ballRadius + sunRadius AndAlso Not collisionOccurred Then
+            ' Ball touches the sun, stop its movement
+            ballAlive = False
+            collisionOccurred = True ' Set the collision flag to prevent multiple pop-ups
+            ' Show message box
+            MessageBox.Show("You died!")
+            ' Reset the ball position
+            ballPosition = New Point(ClientSize.Width / 2, ClientSize.Height / 2)
+            ' Restart the timer after a delay
+            TimerRestart()
+            Me.Close()
+        End If
+
+        Invalidate() ' Redraw the form to update the ball's position
+    End Sub
+
+    Private Sub TimerRestart()
+        timer.Stop() ' Stop the timer
+        collisionOccurred = False ' Reset the collision flag
+        ballAlive = True ' Set the ball to alive again
+
+    End Sub
+
+
+
+End Class
